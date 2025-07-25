52464ceed7c2b035eb5b949ccf441fea5ff50af7
AydanBayramova <bayramoffaaydan1@gmail.com>
2025-07-25 10:20:27 +0400

    Create Exception class for check the age

---
+++ src/main/java/az/edu/java/studentregister/exception/IllegalAgeException.java
@@ -0,0 +1,8 @@
+package az.edu.java.studentregister.exception;
+
+public class IllegalAgeException extends RuntimeException {
+
+    public IllegalAgeException(String message) {
+        super(message);
+    }
+}
