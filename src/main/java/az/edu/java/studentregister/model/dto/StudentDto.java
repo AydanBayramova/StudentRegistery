6bfafd4872108e3e5945fba8d3682e9f16177800
AydanBayramova <bayramoffaaydan1@gmail.com>
2025-07-25 10:20:44 +0400

    CreateDto

---
+++ src/main/java/az/edu/java/studentregister/model/dto/StudentDto.java
@@ -0,0 +1,15 @@
+package az.edu.java.studentregister.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class StudentDto {
+
+    private String name;
+    private String lastName;
+    private LocalDate dateOfBirth;
+}
